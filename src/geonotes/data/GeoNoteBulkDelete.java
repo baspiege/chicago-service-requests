4e6fe52b79456f978ae4e5a19a1943a859ce78d7
spiegel121 <spiegel121@gmail.com>
2011-08-26 02:50:19 +0000

    Upload bulk delete to just delete data from city source for now.

--- src/geonotes/data/GeoNoteBulkDelete.java
+++ src/geonotes/data/GeoNoteBulkDelete.java
@@ -1,5 +1,7 @@
 package geonotes.data;
 
+import java.util.List;
+
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
 import javax.servlet.http.HttpServletRequest;
@@ -23,10 +25,18 @@ public class GeoNoteBulkDelete {
      */
     public void execute(HttpServletRequest aRequest) {
         PersistenceManager pm=null;
+        long count=1;
         try {
             pm=PMF.get().getPersistenceManager();
-            Query query = pm.newQuery(GeoNote.class);
-            query.deletePersistentAll();
+            while (count>0) {
+                Query query = pm.newQuery(GeoNote.class);
+                query.setRange(0,1000); 
+                query.setFilter("(user==userParam)");
+                query.declareParameters("String userParam");                
+                List<GeoNote> notes=(List<GeoNote>) query.execute("CityDataPortal");
+                count=notes.size();
+                pm.deletePersistentAll(notes);
+            }
         } catch (Exception e) {
             System.err.println(this.getClass().getName() + ": " + e);
             e.printStackTrace();
